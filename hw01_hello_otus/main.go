9b1cdcce867b23826accb268f283f2936fb76172
Nikita Suvorov <n.suvorov@tionix.ru>
2021-03-01 08:28:07 +0300

    HW1 is completed

--- hw01_hello_otus/main.go
+++ hw01_hello_otus/main.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"fmt"
+
+	"golang.org/x/example/stringutil"
+)
+
 func main() {
-	// Place your code here.
+	greeting := "Hello, OTUS!"
+	reverseGreeting := stringutil.Reverse(greeting)
+	fmt.Println(reverseGreeting)
 }
