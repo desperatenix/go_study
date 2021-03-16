79da3dc500491fe56513e60319d347c8b915590f
Nikita Suvorov <n.suvorov@tionix.ru>
2021-03-16 13:02:02 +0300

    create Unpack function on elif statement

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,36 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"regexp"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(s string) (string, error) {
+	var prev rune
+	var b strings.Builder
+	errPattern := regexp.MustCompile(`(^\d|\d{2,})`)
+	if errPattern.FindString(s) != "" {
+		return "", ErrInvalidString
+	}
+	if s == "" {
+		return "", nil
+	}
+	for _, cur := range s {
+		if unicode.IsDigit(cur) && string(cur) != "0" { //nolint:gocritic
+			integer, _ := strconv.Atoi(string(cur))
+			b.WriteString(strings.Repeat(string(prev), integer-1))
+		} else if string(cur) == "0" {
+			crop := b.String()[:b.Len()-1]
+			b.Reset()
+			b.WriteString(crop)
+		} else {
+			b.WriteString(string(cur))
+		}
+		prev = cur
+	}
+	return b.String(), nil
 }
