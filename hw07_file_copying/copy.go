9d448ca75748262603851164914eba971381c037
Никита Суворов <nikita.suvorov@j4fun.ru>
2021-07-02 15:42:54 +0300

    hw07 done

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"errors"
+	"github.com/cheggaaa/pb/v3"
+	"io"
+	"os"
 )
 
 var (
@@ -10,6 +13,49 @@ var (
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	info, err := os.Stat(fromPath)
+	if err != nil {
+		return err
+	}
+	if info.IsDir() || !info.Mode().IsRegular() {
+		return ErrUnsupportedFile
+	}
+	if offset >= info.Size() {
+		return ErrOffsetExceedsFileSize
+	}
+
+	src, err := os.OpenFile(fromPath, os.O_RDONLY, 0755)
+	if err != nil {
+		return err
+	}
+	dst, err := os.OpenFile(toPath, os.O_TRUNC|os.O_CREATE|os.O_WRONLY, 0755)
+	if err != nil {
+		return err
+	}
+
+	bytesLeft := info.Size() - offset
+	barSize := limit
+	if bytesLeft < limit || limit == 0 {
+		barSize = bytesLeft
+	}
+
+	bar := pb.Full.Start64(barSize)
+	defer bar.Finish()
+	barReader := bar.NewProxyReader(src)
+
+	_, err = src.Seek(offset, io.SeekStart)
+	if err != nil {
+		return err
+	}
+
+	if limit == 0 {
+		_, err = io.Copy(dst, barReader)
+	} else {
+		_, err = io.CopyN(dst, barReader, limit)
+	}
+	if err != nil && err != io.EOF {
+		return err
+	}
+
 	return nil
 }
